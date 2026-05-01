005f9b7eeb2240f226f1cb9dac3313a6d1b4aa50
unknown <j.d.kotlarski@gmx.com>
2026-05-01 14:35:33 +0200

    somehow the defineModel does not work

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup>
-const de = 0;
+const groceryList = defineModel([
+    {id: 1, name: 'Rijst', price: 1.0, amount: 1},
+    {id: 2, name: 'Broccoli', price: 0.99, amount: 2},
+    {id: 3, name: 'Koekjes', price: 1.2, amount: 4},
+    {id: 4, name: 'Noten', price: 2.99, amount: 0},
+]);
 </script>
 
 <template>
-    <div>
-        {{ de }}
-    </div>
+    <table>
+        <tr>
+            <th>Naam</th>
+            <th>Prijs</th>
+            <th>Aantal</th>
+        </tr>
+        <tr v-for="entry in groceryList" :key="entry.id">
+            <td>{{ name }}</td>
+            <td>{{ entry.value.price }}</td>
+        </tr>
+    </table>
+    <div>{{ groceryList }}</div>
 </template>
