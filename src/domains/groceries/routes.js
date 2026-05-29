fedca5bbbd4e7e44710ece928fb23cbe3081d877
unknown <j.d.kotlarski@gmx.com>
2026-05-29 14:19:44 +0200

    Added some comments

--- src/domains/groceries/routes.js
+++ src/domains/groceries/routes.js
@@ -2,9 +2,9 @@ import Overview from './pages/Overview.vue';
 import Create from './pages/Create.vue';
 import Edit from './pages/Edit.vue';
 
+/// Routes can be defined on their own
 export const routes = [
     {path: '/', component: Overview},
     {path: '/create', component: Create},
-    {path: '/edit',  component: Edit},
+    {path: '/edit', component: Edit},
 ];
-
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import {createMemoryHistory, createRouter} from 'vue-router';
 
 import {routes} from '../domains/groceries/routes.js';
 
+/// Routes are imported and put into the router
 export const router = createRouter({
     history: createMemoryHistory(),
     routes,
