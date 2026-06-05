0e22b2757835f6116d66a649aa68bf1977bf13eb
unknown <j.d.kotlarski@gmx.com>
2026-06-05 15:52:36 +0200

    How to navigate to another page in script setup

--- src/domains/groceries/components/GroceryForm.vue
+++ src/domains/groceries/components/GroceryForm.vue
@@ -1,37 +1,26 @@
 <script setup>
-import {ref} from 'vue';
 const props = defineProps({
     grocery: Object,
 });
 
-const groceryCopy = ref(props.grocery);
+const groceryCopy = props.grocery;
 
-const emit = defineEmits(['submit', 'imagination']);
+const emit = defineEmits(['submit']);
 
 const submitGrocery = () => {
-    emit('submit', groceryCopy.value);
-};
-
-const emitGrocery = () => {
-    emit('imagination', groceryCopy.value);
+    emit('submit', groceryCopy);
 };
 </script>
 
 <template>
-    {{ groceryCopy }}
-
+    <label for="name">Naam:</label>
+    <input type="text" v-model="groceryCopy.name" />
     <br />
+    <label for="price">Prijs:</label>
+    <input type="number" min="0" v-model.number="groceryCopy.price" />
     <br />
-    <form>
-        <label for="name">Naam:</label>
-        <input type="text" id="name" name="name" v-model="groceryCopy.name" />
-        <br />
-        <label for="price">Prijs:</label>
-        <input type="number" id="price" name="price" min="0" v-model.number="groceryCopy.price" />
-        <br />
-        <label for="amount">Aantal:</label>
-        <input type="number" id="amount" name="amount" min="0" v-model.number="groceryCopy.amount" />
-        <br />
-        <button type="button" @click="emitGrocery">Toevoegen</button>
-    </form>
+    <label for="amount">Aantal:</label>
+    <input type="number" min="0" v-model.number="groceryCopy.amount" />
+    <br />
+    <button type="button" @click="submitGrocery">Toevoegen</button>
 </template>
--- src/domains/groceries/pages/Create.vue
+++ src/domains/groceries/pages/Create.vue
@@ -3,37 +3,24 @@ import {ref, computed} from 'vue';
 
 import GroceryForm from '../components/GroceryForm.vue';
 import {getAllGroceries, addGrocery} from '../store.js';
+import {RouterLink} from 'vue-router';
 
 const groceries = ref(getAllGroceries);
 
-const updateID = computed(() => {
-    return groceries.value.length;
-});
-
 const grocery = {
-    id: -1,
+    id: groceries.value.length + 1,
     name: 'Test',
     price: 6,
     amount: 5,
 };
 
 const pushGrocery = groc => {
-    console.log(groc);
-    addGrocery(grocery);
-};
-
-const updateGrocery = () => {
-    console.log(getAllGroceries);
-    addGrocery(grocery);
-    // ++grocery.id;
+    addGrocery(groc);
 };
 </script>
 
 <template>
     Nieuwe boodschap
     <br />
-    <GroceryForm @imagination="pushGrocery" :grocery="grocery" />
-    <br />
-
-    <button @click="updateGrocery(grocery)">Probeer</button>
+    <GroceryForm @submit="pushGrocery" :grocery="grocery" />
 </template>
